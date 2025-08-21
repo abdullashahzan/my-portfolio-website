81d8795c1e1e62456e3ab148b10c49d0c3821d99
abdullashahzan <abdullashahzan@gmail.com>
2025-08-21 16:39:08 +0300

    Added local host in ALLOWED HOSTS

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#^+z7svh_-k-ioprohhwdkjv#oz9^3q1r=uu3i8nrxy93qb*y3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['myportfolioshahzan.pythonanywhere.com']
+ALLOWED_HOSTS = ['myportfolioshahzan.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
