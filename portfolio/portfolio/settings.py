ecf7a047efed3aa9e63bf25bb7b31443b0ca4fd9
abdullashahzan <abdullashahzan@gmail.com>
2025-08-24 20:39:47 +0300

    Debug False
    Debug switched to False

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#^+z7svh_-k-ioprohhwdkjv#oz9^3q1r=uu3i8nrxy93qb*y3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['myportfolioshahzan.pythonanywhere.com', '127.0.0.1']
 
